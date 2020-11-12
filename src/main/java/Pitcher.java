d1b93df5c0bef4161451910d3d81dca8fe8f1ea5
hseungkim <crowhans@gmail.com>
2020-11-13 00:37:27 +0900

    refactor (  컴퓨터 - 3자리 번호를 생성한다. )

--- src/main/java/Pitcher.java
+++ src/main/java/Pitcher.java
@@ -9,18 +9,14 @@ public class Pitcher {
         StringBuffer sb = new StringBuffer();
         List<Integer> initNumber = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
 
-        // 0~8까지 랜덤 숫자
         Random random = new Random();
-        int ran = random.nextInt(9);
-        sb.append(initNumber.get(ran));
-        initNumber.remove(ran);
-
-        ran = random.nextInt(8);
-        sb.append(initNumber.get(ran));
-        initNumber.remove(ran);
-
-        ran = random.nextInt(7);
-        sb.append(initNumber.get(ran));
+        int bound = 9;
+        for(int i=0; i<3; i++){
+            int ran = random.nextInt(bound);
+            sb.append(initNumber.get(ran));
+            initNumber.remove(ran);
+            bound--;
+        }
 
         return sb.toString();
     }
